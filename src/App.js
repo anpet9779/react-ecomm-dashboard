529a19182e1808170adfe69a8bb26a9fc53c9b2e
Parag <anpet9779@gmail.com>
2021-05-16 19:56:56 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecomm-Project</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
